975e91845fb92e3192c28921d3bb7ae42afb33e3
iihimanshuub2 <2121997him@gmail.com>
2023-06-16 13:30:50 +0530

    changes

--- src/components/MainDashboard/widgets/Widgets.jsx
+++ src/components/MainDashboard/widgets/Widgets.jsx
@@ -6,6 +6,7 @@ const Widget = ()=>{
         <div className="w-full flex flex-row justify-between ">
            
            { widgetData.map(items=>(
+            <>
                 <div className="w-60 h-28 flex flex-row rounded-[1.6rem]" style={{background:items.color}}>
                 <div className="w-[80%] h-full flex flex-row">
                     <div className="self-end p-6">
@@ -18,7 +19,7 @@ const Widget = ()=>{
 
                 </div>
             </div>
-
+            </>
            ))
             
           
